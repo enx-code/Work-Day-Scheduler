fd1317862b88bd16bc81f1af9e0d8076b5f78975
Enky <enkh6at@yahoo.com>
2023-12-12 01:41:15 +0000

    adding color by past presen and future

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
 console.log("hello")
 $(document).ready(function () {
     function updateCurrentDay() {
-      const currentDay = dayjs().format("dddd, MMMM D, YYYY");
+      var currentDay = dayjs().format("dddd, MMMM D, YYYY");
       $("#currentDay").text(currentDay);
       return currentDay
     }
     console.log(updateCurrentDay())
 console.log("hello from jquery")
-    
+    function updateColor(){
+        var currentDay = dayjs().hour();
+        $(".hour").each(function(){
+            console.log(parseInt($(this).text().replace("AM", "").replace("PM", ""), 10))
+        })
+    }
+    console.log(updateColor())
 
 });
