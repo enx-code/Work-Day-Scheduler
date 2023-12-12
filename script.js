b5653a81c7a57375837b1ecc212e8adc2f5af8b6
Enky <enkh6at@yahoo.com>
2023-12-12 01:09:37 +0000

    added cuurrent time

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 console.log("hello")
 $(document).ready(function () {
-
-
+    function updateCurrentDay() {
+      const currentDay = dayjs().format("dddd, MMMM D, YYYY");
+      $("#currentDay").text(currentDay);
+      return currentDay
+    }
+    console.log(updateCurrentDay())
+console.log("hello from jquery")
     
+
 });
